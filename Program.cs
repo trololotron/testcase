e878bdbdd8a73f440176d3f91ff1b29af3eba900
trololotron <i.a.ilinov@gmail.com>
2021-06-02 10:41:53 +0700

    робокомерц testcase

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace test_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input path to DLL!");
+            Console.ReadLine();
+            //testcase
+            //var pathToFolder = "C:\\Users\\admin\\source\\repos\\ClassLibrary_test\\ClassLibrary_test\\bin\\Debug\\net5.0";
+            var pathToFolder = Console.ReadLine();
+            PrintClassMethods(pathToFolder);
+        }
+
+        static void PrintClassMethods(string dllPath)
+        {
+            if (Directory.Exists(dllPath))
+            {
+                foreach (string filename in Directory.GetFiles(dllPath))
+                {
+                    if (Path.GetExtension(filename) == ".dll")
+                    {
+                        System.Reflection.Assembly myDllAssembly =
+                            System.Reflection.Assembly.LoadFile(filename);
+
+                        foreach (var type in myDllAssembly.GetTypes())
+                        {
+                            if (!type.IsClass || type.IsNotPublic) continue;
+                            Console.WriteLine("=========================");
+                            Console.WriteLine(type);
+
+                            foreach (var metdh in type.GetMethods())
+                            {
+                                if (metdh.IsPrivate) continue;
+                                Console.WriteLine("- " + metdh.Name);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+                Console.WriteLine("Directory does not exists.");
+
+            Console.WriteLine("End");
+            Console.ReadKey();
+        }
+    }
+}
